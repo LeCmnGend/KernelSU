81f2377922aedb50e1100d479a2fe29738869661
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-21 16:45:25 +0800

    kernel: sucompat: dummify and micro-optimize devpts hook +
    since this is now on LSM, no-op this so
    - builds wont break
    - no double hooking
    
    we also kind of micro-optimize devpts handler
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -1118,7 +1118,7 @@ out_ksu_try_umount:
 	return 0;
 }
 
-extern int ksu_handle_devpts(struct inode *inode); // sucompat.c
+extern int __ksu_handle_devpts(struct inode *inode); // sucompat.c
 
 static int ksu_inode_permission(struct inode *inode, int mask)
 {
@@ -1126,7 +1126,7 @@ static int ksu_inode_permission(struct inode *inode, int mask)
 #ifdef CONFIG_KSU_DEBUG
 		pr_info("%s: devpts inode accessed with mask: %x\n", __func__, mask);
 #endif
-		ksu_handle_devpts(inode);
+		__ksu_handle_devpts(inode);
 	}
 	return 0;
 }
