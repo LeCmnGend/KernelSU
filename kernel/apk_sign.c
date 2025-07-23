59e7f405114d55d7783fd44a525fa94151b7b95a
LeCmnGend <namhoang1235@gmail.com>
2025-07-23 21:50:51 +0700

    susfs: Enable 1.5.9 +
    * Fix some grammar

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -386,8 +386,8 @@ bool ksu_is_manager_apk(char *path)
 		path, expected_manager_size, expected_manager_hash);
 
 #ifdef CONFIG_KSU_SUSFS
-	return (check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
-		check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4") // 5ec1cff
+	return (check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH)
+		|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4") // 5ec1cff
 	);
 #else
 	return check_v2_signature(path, expected_manager_size, expected_manager_hash);
